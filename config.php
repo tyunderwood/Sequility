a64550785a9f4f6c107178e6108ded48b4ad2904
tyunderwood <tyisreally@gmail.com>
2013-01-13 23:05:52 -0500

    delete other stuff

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DB_DRIVER", "mysql");
-define("DB_HOSTNAME", "localhost");
-define("DB_USERNAME", "sequilit_web");
-define("DB_PASSWORD", "B#u!65haFrlW");
-define("DB_DATABASE", "sequilit_sequility");
-define("DB_PREFIX", "amg_");
--- phpinfo.php
+++
@@ -1,7 +0,0 @@
-<?php
-  
-    echo ini_get('date.timezone');
- 
-    echo date("h:00-a e T O");
-    
-    phpinfo();
